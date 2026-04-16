027a77909820807b9cbe1d5bfc39e5bfc3382749
teresia <tereziah.mpaera@student>
2026-04-16 23:28:18 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
+// Import datejs (required for Date.today())
+require('datejs');
 
+function combineUsers(...args) {
+  // Initialize return object
+  const combinedObject = {
+    users: []
+  };
+
+  // Loop through each array in args
+  for (let arr of args) {
+    combinedObject.users.push(...arr);
+  }
+
+  // Add today's date in required format
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+  // Return the object
+  return combinedObject;
+}
+
+// Export function
+module.exports = {
+  combineUsers
+};
 
 
 module.exports = {
